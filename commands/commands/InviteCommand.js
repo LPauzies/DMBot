b96381f620463114228cde7372db2941f0499c78
Cair0n <lucas.pauzies@hotmail.fr>
2020-11-22 14:41:42 +0100

    InviteCommand

---
+++ commands/commands/InviteCommand.js
@@ -0,0 +1,21 @@
+const Command = require('../Command.js');
+
+class InviteCommand extends Command.Command {
+
+    constructor(prefix) {
+        super(prefix, "invite", "Generate invitation link of DMBot");
+    }
+
+    // Overrided from Command
+    execute(messageObject, ...args) {
+        if (args[0]) messageObject.reply(`ere is the link to invite me in another discord server ${args[0]}`);
+    }
+
+    //Overrided from Command
+    help() {
+        return `${this.prefix} ${this.keyword} => ${this.description}`;
+    }
+
+}
+
+exports.InviteCommand = InviteCommand;

f13a803367441b70481435a601c32a299e222e69
Cair0n <lucas.pauzies@hotmail.fr>
2020-12-03 11:17:18 +0100

    HelpCommand

---
+++ commands/commands/HelpCommand.js
@@ -0,0 +1,20 @@
+const Command = require("../Command");
+
+class HelpCommand extends Command.Command {
+
+    constructor(prefix, ...commands) {
+        super(prefix, "help", "Generate help for DMBot");
+        this.commands = "Here is the help of DMBot :\n" + commands.map(command => command.help()).join("\n");
+    }
+
+    // Overrided from Command
+    execute(messageObject, ...args) {
+        messageObject.channel.send(this.commands);
+    }
+
+    //Overrided from Command
+    help() {
+        return `${this.prefix} ${this.keyword} => ${this.description}`;
+    }
+
+}
